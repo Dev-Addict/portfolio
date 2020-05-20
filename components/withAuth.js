e6c54b750e400a7d0327bed067bcd9b7c859f76b
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-21 00:43:36 +0430

    add role to auth

--- components/withAuth.js
+++ components/withAuth.js
@@ -1,13 +1,13 @@
 import BaseLayout from "./BaseLayout";
 
-const withAuth = Component => {
+const withAuth = (role = 'user') => (Component) => {
     const withAuthComponent = (props) => {
-        if (props.auth.isAuthenticated) {
+        if (props.auth.isAuthenticated && props.auth.user['http://localhost:3000/role'].includes(role)) {
             return (<Component {...props}/>);
         }
         return (
             <BaseLayout auth={props.auth}>
-                <h1>You must be signed in to see this page.</h1>
+                <h1>You Are Not Allowed To See This Page.</h1>
             </BaseLayout>
         );
     };
--- server/services/auth.js
+++ server/services/auth.js
@@ -12,3 +12,15 @@ exports.checkJWT = jwt({
     issuer: 'https://dev-ajkpwfyj.auth0.com/',
     algorithms: ['RS256']
 });
+
+exports.checkRole = (role) => (req, res, next) => {
+    const user = req.user;
+
+    if (user && user['http://localhost:3000/role'].includes(role)) {
+        return next();
+    }
+    res.status(401).json({
+        status: 'err',
+        message: `You Need To Be ${role} To Use This Route. ${err.message}`
+    });
+};
