d190e0fddcfe8f5ae2f1f2c13a517acc37e5b0cb
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-22 20:43:07 +0430

    create handler factory

---
+++ server/controllers/handlerFactory.js
@@ -0,0 +1,100 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
+const APIFeatures = require('../utils/APIFeatures');
+
+exports.deleteOne = Model =>
+    catchAsync(
+        async (req, res) => {
+            const doc = await Model.findByIdAndDelete(req.params.id);
+            if (!doc) {
+                throw new AppError('No document found with specified ID', 404);
+            }
+
+            res.status(204).json({
+                status: 'success',
+                data: null
+            });
+        }
+    );
+
+exports.updateOne = Model =>
+    catchAsync(
+        async (req, res) => {
+            const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                runValidators: true
+            });
+
+            if (!doc) {
+                throw new AppError('No document found with that ID', 404);
+            }
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    doc
+                }
+            });
+        }
+    );
+
+exports.createOne = Model =>
+    catchAsync(
+        async (req, res) => {
+            const doc = await Model.create(req.body);
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    doc
+                }
+            });
+        }
+    );
+
+exports.getOne = (Model, populateOptions) =>
+    catchAsync(
+        async (req, res) => {
+            let query = Model.findById(req.params.id);
+            if (populateOptions) {
+                query = query.populate(populateOptions);
+            }
+            const doc = await query;
+            if (!doc) {
+                throw new AppError('No document found with this ID', 404);
+            }
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    doc
+                }
+            });
+        }
+    );
+
+exports.getAll = Model =>
+    catchAsync(
+        async (req, res) => {
+            let filter = {};
+            if (req.params.tourId) {
+                filter = {
+                    tour: req.params.tourId
+                }
+            }
+            const features =
+                new APIFeatures(Model.find(filter), req.query)
+                    .filter()
+                    .sort()
+                    .limitFields()
+                    .paginate();
+            const docs = await features.query;
+            res.status(200).json({
+                status: 'success',
+                results: docs.length,
+                page: features.queryString.page * 1 || 1,
+                limit: features.queryString.limit * 1 || 20,
+                data: {
+                    tours: docs
+                }
+            });
+        }
+    );
