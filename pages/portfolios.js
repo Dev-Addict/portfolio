5f93140cc8a5398949f8607236fc538f97365447
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-22 22:23:59 +0430

    render portfolios

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,25 @@
+import {Col, Card, CardHeader, CardBody, CardTitle, CardText} from 'reactstrap';
+
 import BaseLayout from "../components/BaseLayout";
 import {getPortfolios} from "../actions";
 
 const Portfolios = ({auth, portfolios}) => {
+    const renderPortfolios = () => portfolios.map((portfolio, index) => (
+        <Col md="4" key={index}>
+            <span>
+                <Card className="portfolio-card">
+                    <CardHeader className="portfolio-card-header">{portfolio.position}</CardHeader>
+                    <CardBody>
+                        <p className="portfolio-card-city">{portfolio.location}</p>
+                        <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
+                        <CardText className="portfolio-card-text">{portfolio.description}</CardText>
+                        <div className="readMore"></div>
+                    </CardBody>
+                </Card>
+            </span>
+        </Col>
+    ));
+
     return (
         <BaseLayout auth={auth} title="Portfolios" className="portfolio-page">
         </BaseLayout>
