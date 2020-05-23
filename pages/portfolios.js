5386801cdf468e3527ed0550f391e470ef7b36ef
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-23 20:54:15 +0430

    add buttons to portfolio page

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,12 @@
-import {Col, Card, CardHeader, CardBody, CardTitle, CardText} from 'reactstrap';
+import {Col, Card, CardHeader, CardBody, CardTitle, CardText, Button} from 'reactstrap';
 
 import BaseLayout from "../components/BaseLayout";
 import {getPortfolios} from "../actions";
+import {Router} from '../routes';
 
 const Portfolios = ({auth, portfolios}) => {
     const renderPortfolios = () => portfolios.map((portfolio, index) => (
-        <Col md="4" key={index}>
+        <Col md="4" key={index} className="portfolio-card-hand">
             <span>
                 <Card className="portfolio-card">
                     <CardHeader className="portfolio-card-header">{portfolio.position}</CardHeader>
@@ -13,7 +14,14 @@ const Portfolios = ({auth, portfolios}) => {
                         <p className="portfolio-card-city">{portfolio.location}</p>
                         <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
                         <CardText className="portfolio-card-text">{portfolio.description}</CardText>
-                        <div className="readMore"></div>
+                        <div className="readMore">
+                            <Button color="warning" onClick={() => {
+                                Router.pushRoute(`/portfolioEdit/${portfolio._id}`)
+                            }}>
+                                Edit
+                            </Button>{' '}
+                            <Button color="danger">Delete</Button>
+                        </div>
                     </CardBody>
                 </Card>
             </span>
@@ -22,6 +30,9 @@ const Portfolios = ({auth, portfolios}) => {
 
     return (
         <BaseLayout auth={auth} title="Portfolios" className="portfolio-page">
+            <Button className="button-hand" color="success" onClick={() => Router.pushRoute('/portfolioNew')}>
+                Create Portfolio
+            </Button>
             {renderPortfolios()}
         </BaseLayout>
     );
