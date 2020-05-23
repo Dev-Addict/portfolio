3963e2ce3f418182b1898d3f1f7f4114860e1134
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-24 01:37:34 +0430

    update slate editor

--- components/slateEditor.js
+++ components/slateEditor.js
@@ -23,6 +23,18 @@ const initialValues = Value.fromJSON({
     }
 });
 
+const CodeNode = ({attributes, children}) => (
+    <pre {...attributes}>
+        <code>{children}</code>
+    </pre>
+);
+
+const ParagraphNode = ({attributes, children}) => (
+    <p {...attributes}>
+        {children}
+    </p>
+);
+
 const SlateEditor = () => {
     const [value, setValue] = useState(initialValues);
 
@@ -30,8 +42,31 @@ const SlateEditor = () => {
         setValue(value);
     };
 
+    const renderNode = (props, editor, next) => {
+        if (props.node.type === 'code') {
+            return (
+                <CodeNode {...props}/>
+            );
+        } else if (props.node.type === 'paragraph') {
+            return (<ParagraphNode {...props}/>);
+        }
+        next();
+    };
+
+    const onKeyDown = (event, editor, next) => {
+        if (event.key !== '`' || !event.ctrlKey) {
+            return next();
+        }
+
+        event.preventDefault();
+
+        const isCode = editor.value.blocks.some(block => block.type === 'code');
+
+        editor.setBlocks(isCode ? 'paragraph' : 'code');
+    };
+
     return (
-        <Editor value={value} onChange={onChange}/>
+        <Editor value={value} onChange={onChange} renderNode={renderNode}/>
     );
 };
 
